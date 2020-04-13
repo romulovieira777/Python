5b6e20de3f02c11ff436028679912fcf3b285b59
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-13 10:05:35 -0300

    Aula 2

---
+++ aula2.py
@@ -0,0 +1,9 @@
+a = int(input('Enter the first value: '))
+b = int(input('Enter the second value: '))
+soma = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+rest = a % b
+result = ('Soma: {} \nSubtração: {} \nMultiplição: {} \nDivisão: {} \nResto: {}'.format(soma, subtraction, multiplication, division, rest))
+print(result)
