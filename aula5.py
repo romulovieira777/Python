ac853311a7cc58fc429c1f81182de405b1577ba8
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-13 10:06:46 -0300

    Aula 5

---
+++ aula5.py
@@ -0,0 +1,36 @@
+lista = [12, 10, 7, 5]
+lista_animal = ['Dog', 'Cat', 'Elephant', 'Wolf', 'Macaw']
+lista_animal[0] = 'Monkey'
+print(lista_animal)
+
+
+# tupla = (1, 10, 12, 14)
+# print(len(tupla))
+# print(len(lista_animal))
+# tupla_animal = tuple(lista_animal)
+# print(type(tupla_animal))
+# print(tupla_animal)
+# lista_numeric = list(tupla)
+# print(type(lista_numeric))
+# lista_numeric[0] = 100
+# print(lista_numeric)
+
+
+# print(lista_animal[1])
+# new_lista = lista_animal * 3
+# print(new_lista)
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+# if 'wolf' in lista_animal:
+#     print('There is a wolf in the list.')
+# else:
+#     print('There is no wolf in the list')
+#     lista_animal.append('wolf')
+#     print(lista_animal)
+# lista_animal.pop([0])
+# print(lista_animal)
+# lista_animal.remove('Elephant')
+# print(lista_animal)
+
